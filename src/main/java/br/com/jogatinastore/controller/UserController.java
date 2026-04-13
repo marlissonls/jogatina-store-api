7c6f6d14db95b199f2f827d79c904dffc3a8dc1d
Marlisson Silva <marlisson.business@gmail.com>
2026-04-12 22:15:37 -0300

    feat: add user CRUD endpoint

---
+++ src/main/java/br/com/jogatinastore/controller/UserController.java
@@ -0,0 +1,51 @@
+package br.com.jogatinastore.controller;
+
+import br.com.jogatinastore.model.User;
+import br.com.jogatinastore.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/user/v1")
+public class UserController {
+
+    UserService services;
+
+    public UserController(UserService services) {
+        this.services = services;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+
+        return services.findAll();
+    }
+
+    @GetMapping(path="/{id}")
+    public User findById(@PathVariable UUID id) {
+
+        return services.findById(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+
+        return services.create(user);
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+
+        return services.update(user);
+    }
+
+    @DeleteMapping(path="/{id}")
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
+
+        services.delete(id);
+        return ResponseEntity.noContent().build(); // 204
+    }
+}
---
+++ src/main/java/br/com/jogatinastore/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.jogatinastore.repository;
+
+import br.com.jogatinastore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+
+}
