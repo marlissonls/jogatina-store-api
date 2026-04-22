19c7bada6e1d4764b54379c21174a1a09d3d9814
Marlisson Silva <marlisson.business@gmail.com>
2026-04-22 12:40:16 -0300

    refactor(user-mapper): replace manual ignores with explicit mappings for attributes

--- src/main/java/br/com/jogatinastore/model/user/mapper/UserMapper.java
+++ src/main/java/br/com/jogatinastore/model/user/mapper/UserMapper.java
@@ -1,17 +1,12 @@
 package br.com.jogatinastore.model.user.mapper;
 
 import br.com.jogatinastore.model.user.User;
-import br.com.jogatinastore.model.user.UserPermission;
 import br.com.jogatinastore.model.user.dto.CreateUserDTO;
 import br.com.jogatinastore.model.user.dto.UpdateUserDTO;
 import br.com.jogatinastore.model.user.dto.UserResponseDTO;
 import org.mapstruct.*;
-import org.mapstruct.factory.Mappers;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", uses = PermissionMapperUtils.class)
 public interface UserMapper {
@@ -21,30 +16,24 @@ public interface UserMapper {
 
     List<UserResponseDTO> toResponseList(List<User> users);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "password", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "accountNonExpired", ignore = true)
-    @Mapping(target = "accountNonLocked", ignore = true)
-    @Mapping(target = "credentialsNonExpired", ignore = true)
-    @Mapping(target = "enabled", ignore = true)
-    @Mapping(target = "userPermissions", ignore = true)
+
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "cpf", source = "cpf")
+    @Mapping(target = "phoneNumber", source = "phoneNumber")
+    @Mapping(target = "birthDate", source = "birthDate")
+    @Mapping(target = "email", source = "email")
     User toEntity(CreateUserDTO dto);
 
     List<User> toEntityList(List<CreateUserDTO> dtos);
 
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "email", ignore = true)
-    @Mapping(target = "password", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "cpf", ignore = true)
-    @Mapping(target = "accountNonExpired", ignore = true)
-    @Mapping(target = "accountNonLocked", ignore = true)
-    @Mapping(target = "credentialsNonExpired", ignore = true)
-    @Mapping(target = "enabled", ignore = true)
-    @Mapping(target = "userPermissions", ignore = true)
+
+    @BeanMapping(
+        ignoreByDefault = true,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+    )
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "birthDate", source = "birthDate")
+    @Mapping(target = "phoneNumber", source = "phoneNumber")
     void updateEntity(UpdateUserDTO dto, @MappingTarget User user);
 }
